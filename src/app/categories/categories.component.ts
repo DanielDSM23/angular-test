2eb11de51cb1a3316a3080fb4abe2dd39cd564e7
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2025-11-05 10:41:25 +0100

    add request categories

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../shared/services/categories.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+
+  constructor(private catgoriesService : CategoriesService) { }
+
+  ngOnInit()  {
+    console.log("Hello Categories Component");
+    this.catgoriesService.getCategories().subscribe(categories => {
+      this.catgoriesService.categories = categories;
+      console.log(this.catgoriesService.categories);
+    });
+  }
 
 }
